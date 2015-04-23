1ed636068937bedf4cdfb9369ddeb5c8a574d81c
sam629205 <285360060@qq.com>
2015-04-23 16:58:10 +0800

    4.23 16:57

--- yinyuetai/src/com/personal/yinyuetai/view/LancherLayout.java
+++ yinyuetai/src/com/personal/yinyuetai/view/LancherLayout.java
@@ -461,12 +461,12 @@ public class LancherLayout extends LinearLayout implements ShadowCallBack, View.
 			}
 		}
 		int i = -1;
-		int yoff = 130;
-		int xoff = 152;
+		int activityHeight = ((Activity)context).getWindowManager().getDefaultDisplay().getHeight();
+		double rate = 1080/(double)activityHeight;
+		int yoff = (int) (130/rate);
 		loc = new int[2];
 		loc1 = new int[2];
 		loc2 = new int[2];
-
 		if (listIndex==0) {
 			imageViews[0].getLocationInWindow(loc);
 			imageViews[1].getLocationInWindow(loc1);
