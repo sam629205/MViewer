f5cc163786fd5fe70aca3c2d206031b92f55bf21
sam629205 <285360060@qq.com>
2015-07-31 22:38:45 +0800

    0731

--- yinyuetai/src/com/personal/service/getRealURL.java
+++ yinyuetai/src/com/personal/service/getRealURL.java
@@ -222,7 +222,9 @@ public String parseLink(String URL) {
 	String reaLink = null;
 	
 	try {
-		doc = Jsoup.connect(URL).timeout(20000).get();
+		doc = Jsoup.connect(URL).header("User-Agent", "Mozilla/5.0 (Macintosh; U; "
+				+ "Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/"
+				+ "3.6.2").timeout(20000).get();
 		Elements root = doc.getElementsByTag("a");
 		reaLink = root.get(8).attr("href");
 	} catch (MalformedURLException e) {
